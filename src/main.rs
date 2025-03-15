63ebc5b87362fd2eedc1b6cc4de38780fbf11ef6
Hunters8568 <stasonishu27@northandoverpublicschools.com>
2025-03-15 08:50:37 -0600

    Work on server and packet system

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::net::TcpListener;
 
+use clap::Command;
 use log::*;
+use ringbuffer::RingBuffer;
 
+mod ringbuffer;
 mod server;
 
 const SRBNFS_SERVER_PORT: i32 = 7848;
@@ -9,29 +12,67 @@ const SRBNFS_SERVER_PORT: i32 = 7848;
 fn main() {
     env_logger::init();
 
+    let cmd = Command::new("srbnfs")
+        .about("Stupid Ring Buffer Network Filesystem")
+        .subcommand_required(true)
+        .subcommand(
+            Command::new("rootserver")
+                .about("Boot in root server mode, loading the ring buffer from disk"),
+        );
+
+    let matches = cmd.get_matches();
+
+    let mut enable_root_server = false;
+
+    trace!("Default: root server enabled: {}", enable_root_server);
+
+    match matches.subcommand() {
+        Some(("rootserver", _)) => {
+            info!("Root server mode enabled!");
+            enable_root_server = true;
+        }
+        _ => unreachable!(),
+    }
+
     info!("Welcome to SRBNFS - Stupid Ring Buffer Network Filesystem");
 
-    debug!("Starting remote SRBNFS server...");
+    if enable_root_server {
+        let ring_cfg_content =
+            std::fs::read_to_string("cfg/ring.txt").expect("Failed to read ring config");
 
-    let server = server::Server {
-        listener: TcpListener::bind(format!("0.0.0.0:{}", SRBNFS_SERVER_PORT))
-            .expect("Failed to find to local address"),
-    };
+        let mut ring_split: Vec<&str> = ring_cfg_content.split("\n").collect();
+        ring_split.pop();
 
-    info!(
-        "SRBNFS server started on address 0.0.0.0:{}",
-        SRBNFS_SERVER_PORT
-    );
+        debug!("Loaded ring buffer: {:#?}", ring_split);
 
-    for client in server.listener.incoming() {
-        debug!("Client connected");
+        debug!("Starting remote SRBNFS server...");
 
-        std::thread::spawn(move || {
-            let mut client = server::Client {
-                stream: client.unwrap(),
-            };
+        let server = server::Server {
+            listener: TcpListener::bind(format!("0.0.0.0:{}", SRBNFS_SERVER_PORT))
+                .expect("Failed to find to local address"),
+        };
 
-            client.handle();
-        });
+        info!(
+            "SRBNFS server started on address 0.0.0.0:{}",
+            SRBNFS_SERVER_PORT
+        );
+
+        for client in server.listener.incoming() {
+            debug!("Client connected");
+            let mut new_ring = vec![];
+
+            for ip in ring_split.to_vec() {
+                new_ring.push(ip.to_string());
+            }
+
+            std::thread::spawn(move || {
+                let mut client = server::Client {
+                    stream: client.unwrap(),
+                    ring_buffer: RingBuffer::new(new_ring.to_vec()),
+                };
+
+                client.handle();
+            });
+        }
     }
 }
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,11 +1,16 @@
+pub mod packet;
+
 use log::*;
 use std::io::BufRead;
+
+use crate::ringbuffer::RingBuffer;
 
 pub struct Server {
     pub listener: std::net::TcpListener,
 }
 pub struct Client {
     pub stream: std::net::TcpStream,
+    pub ring_buffer: RingBuffer,
 }
 
 impl Client {
@@ -13,6 +18,14 @@ impl Client {
         debug!("Client handler start!");
 
         let mut stream = std::io::BufReader::new(&self.stream);
+
+        let mut welcome_packet = packet::Packet::new();
+        welcome_packet
+            .params
+            .insert("ProgramName".to_string(), "SRBNFS-Router".to_string());
+
+        let stream1 = &mut self.stream.try_clone().expect("Failed to clone TCPStream");
+        welcome_packet.send_packet(stream1);
 
         loop {
             let mut line_buffer = String::new();
@@ -22,7 +35,7 @@ impl Client {
                 break;
             }
 
-            debug!("{}", line_buffer);
+            debug!("Got packet from client: {}", line_buffer);
         }
     }
 }
