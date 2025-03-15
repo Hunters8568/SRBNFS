67be925fcfae82d98848bf5a237e36f635f3e4f0
antiLimit <webmaster@antilimit.dev>
2025-03-15 14:42:09 -0400

    PANIC MODE WE ARE RUNNING OUT OF TIME

--- srbnfs-web/src/sections/feed.svelte
+++ srbnfs-web/src/sections/feed.svelte
@@ -25,6 +25,32 @@
             return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
         }
 
+        function fadeRow(element) {
+            // var height = 0.1; // initial height
+            // element.style.display = "block";
+            // var timer = setInterval(function () {
+            //     if (height >= 1) {
+            //         clearInterval(timer);
+            //     }
+            //     element.style.opacity = op;
+            //     element.style.filter = "alpha(opacity=" + op * 100 + ")";
+            //     op += op * 0.1;
+            // }, 10);
+            setTimeout(function () {
+                var op = 1; // initial opacity
+                var timer = setInterval(function () {
+                    if (op <= 0.1) {
+                        clearInterval(timer);
+                        element.style.display = "none";
+                        element.remove();
+                    }
+                    element.style.opacity = op;
+                    element.style.filter = "alpha(opacity=" + op * 100 + ")";
+                    op -= op * 0.1;
+                }, 50);
+            }, 4000);
+        }
+
         function appendFileList(id, name, blob) {
             totalFiles++;
             fileList.unshift({
@@ -34,26 +60,20 @@
 
             let element = document.createElement("tr");
             element.id = `file-${totalFiles.toString()}`;
+            element.onload = fadeRow(element);
             element.innerHTML = `
-            <tr>
-                <th>${id}</th>
-                <th>${name}</th>
-                <th>type</th>
-                <th>${formatBytes(20010)}</th>
-                <th>
-                    <a download="download" href="url">
-                        <button class="download-btn"></button>
-                    </a>
-                </th>
-            </tr>
+            <th>${id}</th>
+            <th>${name}</th>
+            <th>${formatBytes(20010)}</th>
+            <th>
+                <a download="download" href="url">
+                    <button class="download-btn"></button>
+                </a>
+            </th>
             `;
 
-            document
-                .getElementById("feed-table")
-                .insertBefore(
-                    element,
-                    document.getElementById("feed-table").firstChild,
-                );
+            let tableBody = document.getElementById("feed-table").lastChild;
+            tableBody.insertBefore(element, tableBody.firstChild);
 
             if (fileList.length >= 15) {
                 fileList.length = 15;
@@ -61,7 +81,13 @@
         }
 
         for (let i = 0; i < 5; i++) {
-            appendFileList(125, "aaa.txt", new Blob());
+            setTimeout(
+                function () {
+                    appendFileList(125, "aaa.txt", new Blob());
+                },
+                2000 * i,
+                i,
+            );
         }
     };
 </script>
@@ -74,7 +100,6 @@
             <tr>
                 <th>ID</th>
                 <th>Name</th>
-                <th>Type</th>
                 <th>Size</th>
                 <th>Download</th>
             </tr>
@@ -87,5 +112,20 @@
     main {
         border: 3px solid mediumorchid;
         border-radius: 10px;
+    }
+
+    @keyframes file-fade-row {
+        0% {
+            opacity: 100%;
+            height: 100%;
+        }
+        80% {
+            opacity: 100%;
+            height: 100%;
+        }
+        100% {
+            opacity: 0%;
+            height: 0%;
+        }
     }
 </style>
