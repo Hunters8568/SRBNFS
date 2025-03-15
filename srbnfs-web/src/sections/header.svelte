9857fb0ab1b2189e976a2d986bc7b6fa89abf239
antiLimit <webmaster@antilimit.dev>
2025-03-15 11:37:01 -0400

    some stuff im not being very productive

---
+++ srbnfs-web/src/sections/header.svelte
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<main>
+    <h1>SRBNFS</h1>
+</main>
+
+<style>
+    h1 {
+        font-family: "Inter", sans-serif;
+    }
+</style>
---
+++ srbnfs-web/src/sections/transfer.svelte
@@ -0,0 +1,26 @@
+<script>
+</script>
+
+<main>
+    <div id="root-ip">
+        <h2>Root Server Setup</h2>
+
+        <p>Connection Status: Disconnected</p>
+        <p></p>
+
+        <input
+            type="text"
+            name="Root Server IP"
+            id="root-ip-input"
+            placeholder="XX.XX.XX.XX"
+        />
+        <input type="button" value="Connect" id="root-ip-submit" />
+    </div>
+
+    <div id="file-upload">
+        <input type="file" name="Upload File" id="file-upload-input" />
+    </div>
+</main>
+
+<style>
+</style>
